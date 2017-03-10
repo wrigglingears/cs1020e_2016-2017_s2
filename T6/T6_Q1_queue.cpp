2e82ab69a9f0a884b8a2efe785a5eb985f759f2b
Mattheus Lee <mattheus.lee@gmail.com>
2017-03-10 14:19:10 +0800

    started T6

---
+++ T6/T6_Q1_queue.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <queue>
+#include <utility>
+
+class my_queue {
+private:
+    stack<int> inbox;
+    stack<int> outbox;
+
+public:
+    // inefficient enqueue
+    void enqueue(int item) {
+
+    }
+
+    // efficent dequeue
+    int dequeue() {
+
+    }
+};
+
+class my_queue {
+private:
+    stack<int> inbox;
+    stack<int> outbox;
+
+public:
+    // efficent enqueue
+    void enqueue(int item) {
+
+    }
+
+    // inefficient dequeue
+    int dequeue() {
+
+    }
+};
---
+++ T6/T6_Q1_stack.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <queue>
+#include <utility>
+
+class my_stack {
+private:
+    queue<int> _q1;
+    queue<int> _q2;
+    queue<int>* _pushq;
+    queue<int>* _popq;
+
+public:
+    my_stack() {
+
+    }
+
+    // inefficient push
+    void push(int a) {
+
+    }
+
+    // efficient pop
+    void pop() {
+
+    }
+};
+
+class my_stack {
+private:
+    queue<int> _q1;
+    queue<int> _q2;
+    queue<int>* _pushq;
+    queue<int>* _popq;
+
+public:
+    my_stack() {
+
+    }
+
+    // efficient push
+    void push(int a) {
+
+    }
+
+    // inefficient pop
+    void pop() {
+
+    }
+};
